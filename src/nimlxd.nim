e6110cd29d01360155e8d69498c25c7543ef46b6
ghost <ghost@nemesis.sh>
2022-01-19 22:08:37 -0500

    added a test for profiles

--- src/nimlxd.nim
+++ src/nimlxd.nim
@@ -77,14 +77,14 @@ proc test_profile() =
         "flask",
         config = {
             "user.user-data": """
-#cloud-config
-package_update: true
-packages:
-  - python3
-  - python3-pip
-runcmd:
-  - pip install flask
-  - bash /setup.d/entrypoint.sh
+                #cloud-config
+                package_update: true
+                packages:
+                  - python3
+                  - python3-pip
+                runcmd:
+                  - pip install flask
+                  - bash /setup.d/entrypoint.sh
             """
         }.toTable,
         devices = {
